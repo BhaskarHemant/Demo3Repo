b1d059eb7790f5fbf8e5686ea87957bba2cf9fe5
HDB <abc@gmail.com>
2020-05-09 21:00:07 -0700

    local changes to App

--- D3/src/main/java/RxJava/D3/App.java
+++ D3/src/main/java/RxJava/D3/App.java
@@ -1,13 +1,14 @@
 package RxJava.D3;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public void mLocal() {
+    	System.out.println("local");
     }
 }
